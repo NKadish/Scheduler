1d593ea4e9d1243e0a460c72143a3f2b6b73320d
NKadish <noahkadish@hotmail.com>
2020-10-26 17:39:05 -0400

    add tests for DayListItem and a formatSpots function to turn the spots into a string based on how many there are

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,6 +2,17 @@ import React from "react";
 import "components/DayListItem.scss";
 const classnames = require('classnames');
 
+const formatSpots = (spots) => {
+  if (spots === 0) {
+    return 'no spots remaining';
+  } else if (spots === 1) {
+    return `${spots} spot remaining`;
+  } else {
+    return `${spots} spots remaining`;
+  }
+  // return spots ? `${spots} spots remaining` : 'No spots remaining';
+}
+
 export default function DayListItem(props) {
 
   const dayListItemClass = classnames("day-list__item", {
@@ -12,7 +23,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
