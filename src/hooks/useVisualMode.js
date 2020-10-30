9cb8c8f8e25768e8c88dae6fe1e7860a0e7edf92
NKadish <noahkadish@hotmail.com>
2020-10-29 20:59:01 -0400

    add hooks and tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    
+    if (replace === true) {
+      const newHistory = [...history]
+      newHistory.splice(history.length - 1, 1, newMode)
+      setHistory(newHistory);
+      setMode(newMode);
+    } else if (replace === false) {
+      setHistory([...history, newMode]);
+      setMode(newMode);
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      const newMode = history[history.length - 2];
+      const newHistory = history.slice(0, history.length - 1);
+      setMode(newMode);
+      setHistory(newHistory);
+    }
+  }
+
+  return { mode, transition, back, history };
+}
